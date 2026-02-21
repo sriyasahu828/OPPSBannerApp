2027025770037f435a982765009c8850bc798750
Sriya Sahu <sriyasahu828@gmail.com>
2026-02-21 07:06:12 +0530

    UC3:PrintOOPS

---
+++ OPPSBannerApp.java
@@ -0,0 +1,16 @@
+public class OPPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("   ", "   **   ", "   **   ", " ********** ", "   ********* "));
+        System.out.println(String.join("   ", " **  ** ", " **  ** ", " **      ** ", " **         "));
+        System.out.println(String.join("   ", "*      *", "*      *", " **      ** ", "*           "));
+        System.out.println(String.join("   ", "*      *", "*      *", " **      ** ", "*           "));
+        System.out.println(String.join("   ", "*      *", "*      *", " ********** ", " ********   "));
+        System.out.println(String.join("   ", "*      *", "*      *", " ********** ", "         ** "));
+        System.out.println(String.join("   ", "*      *", "*      *", " **         ", "           *"));
+        System.out.println(String.join("   ", "*      *", "*      *", " **         ", "           *"));
+        System.out.println(String.join("   ", " **  ** ", " **  ** ", " **         ", "         ** "));
+        System.out.println(String.join("   ", "   **   ", "   **   ", " **         ", "*********   "));
+    }
+}
