8155d3b249464dcb9c2df19f74ce3d2cbf772a1f
Sriya Sahu <sriyasahu828@gmail.com>
2026-03-17 08:31:08 +0530

    UC8:PrintOOPS

---
+++ OPPSBannerApp.java
@@ -0,0 +1,69 @@
+import java.util.HashMap;
+
+public class OPPSBannerApp {
+
+    // Creates a HashMap containing ASCII art patterns
+    public static HashMap<Character, String[]> createCharacterMap() {
+
+        HashMap<Character, String[]> charMap = new HashMap<>();
+
+        // Pattern for O
+        charMap.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        // Pattern for P
+        charMap.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+
+        // Pattern for S
+        charMap.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+
+        return charMap;
+    }
+
+    // Displays banner message
+    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
+
+        int patternHeight = charMap.get('O').length;
+
+        // Loop through each row
+        for (int line = 0; line < patternHeight; line++) {
+
+            StringBuilder sb = new StringBuilder();
+
+            // Loop through each character
+            for (char ch : message.toCharArray()) {
+                String[] pattern = charMap.get(ch);
+                sb.append(pattern[line]).append("  ");
+            }
+
+            System.out.println(sb.toString());
+        }
+    }
+
+    // Main Method
+    public static void main(String[] args) {
+
+        HashMap<Character, String[]> charMap = createCharacterMap();
+
+        String message = "OOPS";
+
+        displayBanner(message, charMap);
+    }
+}
